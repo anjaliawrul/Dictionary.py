373a86524aacda5eddb2060749a0db05eb80b0fd
anjaliawrul <111339784+anjaliawrul@users.noreply.github.com>
2023-02-14 16:54:25 +0530

    Add files via upload

---
+++ adding items.py
@@ -0,0 +1,5 @@
+# Adding an item to the dictionary is done by using a new index key and assigning a value to it:
+
+a={"anjali":"supriya","manisha":"bhakti","monika":"super"}
+a["color"]="red"
+print(a)
